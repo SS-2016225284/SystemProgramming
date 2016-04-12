1d637fb79311da5901af7b1ee03d4a2c55f1deec
Wooseok <mell03@naver.com>
2016-04-12 20:35:09 +0900

    damn?

--- Ex4/2016225284_PE4.c
+++ Ex4/2016225284_PE4.c
@@ -1,11 +1,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int main (int argc, char**argv) {
     struct stat srcStat;
     struct stat dstStat;
-
+    
+    printf("%d, %d\n", sizeof(argv[1]) / sizeof(argv[1][0]), sizeof(argv[2]) /sizeof(argv[2][0]));
     if(argc != 3) {
         fprintf(stderr, "usage : 2016225284_PE4 SourceFile DestinationFile\n");
         exit(0);
@@ -15,13 +17,17 @@ int main (int argc, char**argv) {
         fprintf(stderr, "SourceFile should be a file\n");
     }
 
-    if(stat(argc[2], &dstStat) == -1) {
-        //? 
+    //no file exists
+    if(stat(argv[2], &dstStat) == -1) {
+        //is it directory?
+        //or is it new file destination?
     }else {
-        fprintf(stderr, "No such directory %s\n", argc[2]);
+        fprintf(stderr, "No such directory %s\n", argv[2]);
     }
 
     //int rename(const char *old, const char * new)
     //open, close read, write
+    //getcwd(char *buf, size_t size), which gets current directory.
+    //chdir(char*)
     return 0;
 }
